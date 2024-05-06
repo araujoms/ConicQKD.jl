c9d656bab30b467ed718fd84dc121b7bf7df495d
araujoms <maltusan@gmail.com>
2024-05-06 18:13:26 +0200

    change type syntax

--- examples/bb84.jl
+++ examples/bb84.jl
@@ -21,199 +21,197 @@ function gmap(ρ, p_z, η0, η1; T = Float64)
     return Gρ
 end
 
-function zmap(ρ; T = Float64)
-    K = zkraus(;T)
+function zmap(ρ)
+    K = zkraus()
     Zρ = sum(K[i] * ρ * K[i] for i = 1:2)
     return Zρ
 end
 
-function zkraus(;T = Float64)
-    Z = [proj(1, 2; T), proj(2, 2; T)]    
+function zkraus()
+    Z = [proj(1, 2), proj(2, 2)]
     K = [kron(Z[i], I(2)) for i = 1:2]
     return K
 end
 
-function corr(ρ; T = Float64)
-    Z = [proj(1, 2; T), proj(2, 2; T)]
-    X = [Complex{T}(0.5) * [1 1; 1 1], Complex{T}(0.5) * [1 -1; -1 1]]
-    QZ = kron(Z[1],Z[2]) + kron(Z[2],Z[1])
-    QX = kron(X[1],X[2]) + kron(X[2],X[1])
+function corr(ρ)
+    Z = [proj(1, 2), proj(2, 2)]
+    X = [0.5 * [1 1; 1 1], 0.5 * [1 -1; -1 1]]
+    QZ = kron(Z[1], Z[2]) + kron(Z[2], Z[1])
+    QX = kron(X[1], X[2]) + kron(X[2], X[1])
     global_basis = Hermitian.([QZ, QX])
     return real(dot.(Ref(ρ), global_basis))
 end
 
-total_rate_bb84(qz, qx; T = Float64) = hae_bb84(qz,qx; T) - hab_bb84(qz,qx; T)
+total_rate_bb84(::Type{T}, qz, qx) where {T} = hae_bb84(T, qz, qx) - hab_bb84(qz, qx)
+total_rate_bb84(qz, qx) = total_rate_bb84(ComplexF64, qz, qx)
 
-hae_bb84_analytic(qz,qx) = (1 - binary_entropy(qx))
-hab_bb84(qz,qx;T) = (1 - binary_entropy(qz))
-total_rate_bb84_analytic(qz, qx; T = Float64) = hae_bb84_analytic(qz,qx;T) - hab_bb84(qz,qx;T)
+hae_bb84_analytic(qz, qx) = (1 - binary_entropy(qx))
+hab_bb84(qz, qx) = binary_entropy(qz)
+total_rate_bb84_analytic(qz, qx) = hae_bb84_analytic(qz, qx) - hab_bb84(qz, qx)
 
-function hae_bb84(qz,qx; T = Float64)
+function hae_bb84(::Type{T}, qz::Real, qx::Real) where {T}
     if qx == 0 && qz == 0
-        return T(1)
+        return real(T(1))
     elseif qz == 0
-        return hae_bb84_reducedz(qx; T = Float64)
+        return hae_bb84_reducedz(T, qx)
     elseif qx == 0
-        return hae_bb84_reducedx(qz; T = Float64)
+        return hae_bb84_reducedx(T, qz)
     else
-        return hae_bb84_general(qz,qx; T = Float64)
+        return hae_bb84_general(T, qz, qx)
     end
 end
+hae_bb84(qz::Real, qx::Real) = hae_bb84(ComplexF64, qz, qx)
 
-function hae_bb84_general(qz,qx; T = Float64)
-    #R = Complex{T}
-    R = T
-    is_complex = R <: Complex
-    
-    qz, qx = T.((qz, qx))
-    model = GenericModel{T}()
+function hae_bb84_general(::Type{T}, qz, qx) where {T}
+    R = real(T)
+    is_complex = T <: Complex
+
+    qz, qx = R.((qz, qx))
+    model = GenericModel{R}()
     if is_complex
         @variable(model, ρ[1:4, 1:4], Hermitian)
     else
         @variable(model, ρ[1:4, 1:4], Symmetric)
     end
-    corr_rho = corr(ρ; T)
+    corr_rho = corr(ρ)
     @constraint(model, corr_rho .== [qz, qx])
-    @constraint(model, tr(ρ) == T(1))
+    @constraint(model, tr(ρ) == 1)
 
     Ghat = [I(4)]
-    Zhat = zkraus(;T)
+    Zhat = zkraus()
 
-    side = size(ρ,1)
-    vec_dim = Cones.svec_length(R, side)
-    ρ_vec = Vector{GenericAffExpr{T,GenericVariableRef{T}}}(undef, vec_dim)
+    side = size(ρ, 1)
+    vec_dim = Cones.svec_length(T, side)
+    ρ_vec = Vector{GenericAffExpr{R,GenericVariableRef{R}}}(undef, vec_dim)
     if is_complex
-        Cones._smat_to_svec_complex!(ρ_vec, T(1)*ρ, sqrt(T(2)))
+        Cones._smat_to_svec_complex!(ρ_vec, T(1) * ρ, sqrt(R(2)))
     else
-        Cones.smat_to_svec!(ρ_vec, T(1)*ρ, sqrt(T(2)))
+        Cones.smat_to_svec!(ρ_vec, T(1) * ρ, sqrt(R(2)))
     end
 
     @variable(model, h)
-    @objective(model, Min, h / log(T(2)))
-    @constraint(model, [h; ρ_vec] in EpiQKDTriCone{T,R}(Ghat,Zhat,1 + vec_dim))
+    @objective(model, Min, h / log(R(2)))
+    @constraint(model, [h; ρ_vec] in EpiQKDTriCone{R,T}(Ghat, Zhat, 1 + vec_dim))
 
-    set_optimizer(model, Hypatia.Optimizer{T})
+    set_optimizer(model, Hypatia.Optimizer{R})
     set_attribute(model, "verbose", true)
     optimize!(model)
     return objective_value(model)
 end
 
-isometryz(; T=Float64) = [[1,0,0,1] [1,0,0,-1]]/sqrt(T(2))
-#isometryz2(; T=Float64) = T(1)*[[1,0,0,0] [0,0,0,1]]
+isometryz(::Type{T}) where {T} = [[1, 0, 0, 1] [1, 0, 0, -1]] / sqrt(T(2))
+#isometryz2() = [[1,0,0,0] [0,0,0,1]]
 
-function hae_bb84_reducedz(qx; T = Float64)
-    #R = Complex{T}
-    R = T
-    is_complex = R <: Complex
-    
-    qx = T(qx)
-    model = GenericModel{T}()
+function hae_bb84_reducedz(::Type{T}, qx) where {T}
+    R = real(T)
+    is_complex = T <: Complex
+
+    qx = R(qx)
+    model = GenericModel{R}()
     if is_complex
         @variable(model, ρ[1:2, 1:2], Hermitian)
     else
         @variable(model, ρ[1:2, 1:2], Symmetric)
     end
-    @constraint(model, ρ[2,2] == qx) #V'*QX*V == proj(2,2)
-    @constraint(model, tr(ρ) == T(1))
+    @constraint(model, ρ[2, 2] == qx) #V'*QX*V == proj(2,2)
+    @constraint(model, tr(ρ) == 1)
 
-    V = isometryz(;T)
+    V = isometryz(T)
     Ghat = [I(2)]
-    Z = zkraus(;T)
-    V2 = T(1)*[[1,0,0,0] [0,0,0,1]]
-    Zhat = [V2'Zi*V for Zi in Z]
+    Z = zkraus()
+    V2 = [[1, 0, 0, 0] [0, 0, 0, 1]]
+    Zhat = [V2'Zi * V for Zi in Z]
 
-    side = size(ρ,1)
-    vec_dim = Cones.svec_length(R, side)
-    ρ_vec = Vector{GenericAffExpr{T,GenericVariableRef{T}}}(undef, vec_dim)
+    side = size(ρ, 1)
+    vec_dim = Cones.svec_length(T, side)
+    ρ_vec = Vector{GenericAffExpr{R,GenericVariableRef{R}}}(undef, vec_dim)
     if is_complex
-        Cones._smat_to_svec_complex!(ρ_vec, T(1)*ρ, sqrt(T(2)))
+        Cones._smat_to_svec_complex!(ρ_vec, T(1) * ρ, sqrt(R(2)))
     else
-        Cones.smat_to_svec!(ρ_vec, T(1)*ρ, sqrt(T(2)))
+        Cones.smat_to_svec!(ρ_vec, T(1) * ρ, sqrt(R(2)))
     end
 
     @variable(model, h)
-    @objective(model, Min, h / log(T(2)))
-    @constraint(model, [h; ρ_vec] in EpiQKDTriCone{T,R}(Ghat,Zhat,1 + vec_dim))
+    @objective(model, Min, h / log(R(2)))
+    @constraint(model, [h; ρ_vec] in EpiQKDTriCone{R,T}(Ghat, Zhat, 1 + vec_dim))
 
-    set_optimizer(model, Hypatia.Optimizer{T})
+    set_optimizer(model, Hypatia.Optimizer{R})
     set_attribute(model, "verbose", true)
     optimize!(model)
     return objective_value(model)
 end
 
-isometryx(; T=Float64) = [[1,0,0,1] [0,1,1,0]]/sqrt(T(2))
-#isometryx2(; T=Float64) = T(0.5)*[[1,1,1,1] [1,-1,-1,1]]
+isometryx(::Type{T}) where {T} = [[1, 0, 0, 1] [0, 1, 1, 0]] / sqrt(T(2))
+#isometryx2() = 0.5 * [[1,1,1,1] [1,-1,-1,1]]
 
-function hae_bb84_reducedx(qz; T = Float64)
-    #R = Complex{T}
-    R = T
-    is_complex = R <: Complex
-    
-    qz = T(qz)
-    model = GenericModel{T}()
+function hae_bb84_reducedx(::Type{T}, qz) where {T}
+    R = real(T)
+    is_complex = T <: Complex
+
+    qz = R(qz)
+    model = GenericModel{R}()
     if is_complex
         @variable(model, ρ[1:2, 1:2], Hermitian)
     else
         @variable(model, ρ[1:2, 1:2], Symmetric)
     end
-    
-    @constraint(model, ρ[2,2] == qz) #V'*QZ*V == proj(2,2)
-    @constraint(model, tr(ρ) == T(1))
 
-    V = isometryx(;T)
+    @constraint(model, ρ[2, 2] == qz) #V'*QZ*V == proj(2,2)
+    @constraint(model, tr(ρ) == 1)
+
+    V = isometryx(T)
     Ghat = [I(2)]
-    Z = zkraus(;T)
-    Zhat = [Zi*V for Zi in Z]
+    Z = zkraus()
+    Zhat = [Zi * V for Zi in Z]
 
-    side = size(ρ,1)
-    vec_dim = Cones.svec_length(R, side)
-    ρ_vec = Vector{GenericAffExpr{T,GenericVariableRef{T}}}(undef, vec_dim)
+    side = size(ρ, 1)
+    vec_dim = Cones.svec_length(T, side)
+    ρ_vec = Vector{GenericAffExpr{R,GenericVariableRef{R}}}(undef, vec_dim)
     if is_complex
-        Cones._smat_to_svec_complex!(ρ_vec, T(1)*ρ, sqrt(T(2)))
+        Cones._smat_to_svec_complex!(ρ_vec, T(1) * ρ, sqrt(R(2)))
     else
-        Cones.smat_to_svec!(ρ_vec, T(1)*ρ, sqrt(T(2)))
+        Cones.smat_to_svec!(ρ_vec, T(1) * ρ, sqrt(R(2)))
     end
 
     @variable(model, h)
-    @objective(model, Min, h / log(T(2)))
-    @constraint(model, [h; ρ_vec] in EpiQKDTriCone{T,R}(Ghat,Zhat,1 + vec_dim))
+    @objective(model, Min, h / log(R(2)))
+    @constraint(model, [h; ρ_vec] in EpiQKDTriCone{R,T}(Ghat, Zhat, 1 + vec_dim))
 
-    set_optimizer(model, Hypatia.Optimizer{T})
+    set_optimizer(model, Hypatia.Optimizer{R})
     set_attribute(model, "verbose", true)
     optimize!(model)
     return objective_value(model)
 end
 
-isometryxz(; T=Float64) = [1,0,0,1]/sqrt(T(2))
+isometryxz(::Type{T}) where {T} = [1, 0, 0, 1] / sqrt(T(2))
 
-function hae_bb84_reducedxz(; T = Float64)
-    #R = Complex{T}
-    R = T
-    is_complex = R <: Complex
-    
-    model = GenericModel{T}()
-    ρ = fill(R(1), (1,1))
-    
-    V = isometryxz(;T)
-    Ghat = [fill(R(1), (1,1))]
-    Z = zkraus(;T)
-    V2 = T(1)*[[1,0,0,0] [0,0,0,1]]
-    Zhat = [V2'Zi*V for Zi in Z]
+function hae_bb84_reducedxz(::Type{T}) where {T}
+    R = real(T)
+    is_complex = T <: Complex
 
-    side = size(ρ,1)
+    model = GenericModel{R}()
+    ρ = fill(T(1), (1, 1))
+
+    V = isometryxz()
+    Ghat = [fill(T(1), (1, 1))]
+    Z = zkraus()
+    V2 = [[1, 0, 0, 0] [0, 0, 0, 1]]
+    Zhat = [V2'Zi * V for Zi in Z]
+
+    side = size(ρ, 1)
     vec_dim = Cones.svec_length(R, side)
-    ρ_vec = Vector{T}(undef, vec_dim)
+    ρ_vec = Vector{R}(undef, vec_dim)
     if is_complex
-        Cones._smat_to_svec_complex!(ρ_vec, T(1)*ρ, sqrt(T(2)))
+        Cones._smat_to_svec_complex!(ρ_vec, T(1) * ρ, sqrt(R(2)))
     else
-        Cones.smat_to_svec!(ρ_vec, T(1)*ρ, sqrt(T(2)))
+        Cones.smat_to_svec!(ρ_vec, T(1) * ρ, sqrt(R(2)))
     end
 
     @variable(model, h)
-    @objective(model, Min, h / log(T(2)))
-    @constraint(model, [h; ρ_vec] in EpiQKDTriCone{T,R}(Ghat,Zhat,1 + vec_dim))
+    @objective(model, Min, h / log(R(2)))
+    @constraint(model, [h; ρ_vec] in EpiQKDTriCone{R,T}(Ghat, Zhat, 1 + vec_dim))
 
-    set_optimizer(model, Hypatia.Optimizer{T})
+    set_optimizer(model, Hypatia.Optimizer{R})
     set_attribute(model, "verbose", true)
     optimize!(model)
     return objective_value(model)
--- examples/mub_complex.jl
+++ examples/mub_complex.jl
@@ -6,26 +6,6 @@ import Hypatia
 import Hypatia.Cones
 import JLD2
 
-"Generates complete set of MUBs for prime `d`"
-function prime_mub(d)
-    U = [zeros(ComplexF64, d, d) for _ = 1:d+1]
-    U[1] = I(d)
-
-    if d == 2
-        U[2] = [1 1; 1 -1] / sqrt(2)
-        U[3] = [1 1; im -im] / sqrt(2)
-    else
-        ω = exp(im * 2 * π / d)
-        for k = 0:d-1, t = 0:d-1, j = 0:d-1
-            exponent = mod(-j * t + k * div(j * (j - 1), 2), d)
-            U[k+2][j+1, t+1] = ω^exponent / sqrt(d)
-        end
-    end
-
-    cleanup!.(U)
-    return U
-end
-
 function numerical_mub(d)
     mub_dict = JLD2.load("mubs.jld2")
     return mub_dict["mubs"][d]
@@ -33,15 +13,13 @@ end
 
 "Decoherence map acting on Alice's key storage"
 function zgmap(rho::AbstractMatrix, d::Integer)
-    proj_comp = [ket(i, d) * ket(i, d)' for i = 1:d]
-    K = [kron(proj_comp[i], I(d)) for i = 1:d]
+    K = zgkraus(d)
     zgrho = sum(K[i] * rho * K[i] for i = 1:d)
     return Hermitian(zgrho)
 end
 
-function zgkraus(d::Integer, R)
-    proj_comp = [proj(i, d; R) for i = 1:d]
-    K = [kron(proj_comp[i], I(d)) for i = 1:d]
+function zgkraus(d::Integer)
+    K = [kron(proj(i, d), I(d)) for i = 1:d]
     return K
 end
 
@@ -68,7 +46,10 @@ function mub_rate_analytic(v::Real, d::Integer, n::Integer)
     if n == 2
         return log2(d) + Q * log2(Q / (d - 1)) + (1 - Q) * log2(1 - Q)
     elseif n == d + 1
-        return log2(d) + (1 - (d + 1) * Q / d) * (log2(1 - Q - Q / d) - log2(1 - Q)) + (Q / d) * (log2(Q / (d^2 - d)) - log2(1 - Q)) + Q * log2(1 / d)
+        return log2(d) +
+               (1 - (d + 1) * Q / d) * (log2(1 - Q - Q / d) - log2(1 - Q)) +
+               (Q / d) * (log2(Q / (d^2 - d)) - log2(1 - Q)) +
+               Q * log2(1 / d)
     else
         error("n must be either 2 or d+1")
     end
@@ -80,15 +61,13 @@ hab(v, d) = binary_entropy(v + (1 - v) / d) + (1 - v - (1 - v) / d) * log2(d - 1
 "Computes the conditional entropy H(A|E) numerically for an isotropic state of dimension `d` with visibility `v`, using `n` MUBs.
 
 Note that `d` must be a prime number, and 2 ≤ `n` ≤ `d` + 1"
-function mub_rate(v::Real, d::Integer, n::Integer)
-    T = Float64
-    R = Complex{T}
-    #    R = T
-    is_complex = (R <: Complex)
+function mub_rate(::Type{T}, v::Real, d::Integer, n::Integer) where {T}
+    R = real(T)
+    is_complex = (T <: Complex)
 
-    v = T(v)
+    v = R(v)
     W = v + (1 - v) / d
-    model = GenericModel{T}()
+    model = GenericModel{R}()
     if is_complex
         @variable(model, rho[1:d^2, 1:d^2], Hermitian)
     else
@@ -97,27 +76,28 @@ function mub_rate(v::Real, d::Integer, n::Integer)
     corr_rho = corr(rho, bases(d, n))
     corr_iso = W * ones(n)
     JuMP.@constraint(model, corr_rho .== corr_iso)
-    JuMP.@constraint(model, tr(rho) == T(1))
+    JuMP.@constraint(model, tr(rho) == 1)
     side::Int = size(rho, 2)
-    vec_dim = Cones.svec_length(R, side)
-    rho_vec = Vector{JuMP.AffExpr}(undef, vec_dim)
+    vec_dim = Cones.svec_length(T, side)
+    rho_vec = Vector{GenericAffExpr{R,GenericVariableRef{R}}}(undef, vec_dim)
 
     if is_complex
-        Cones._smat_to_svec_complex!(rho_vec, T(1) * rho, sqrt(T(2)))
+        Cones._smat_to_svec_complex!(rho_vec, T(1) * rho, sqrt(R(2)))
     else
-        Cones.smat_to_svec!(rho_vec, T(1) * rho, sqrt(T(2)))
+        Cones.smat_to_svec!(rho_vec, T(1) * rho, sqrt(R(2)))
     end
 
     G = [I(d^2)]
-    ZG = zgkraus(d, R)
+    ZG = zgkraus(d)
 
     JuMP.@variable(model, h)
-    JuMP.@objective(model, Min, h / log(T(2)))
-    JuMP.@constraint(model, [h; rho_vec] in EpiQKDTriCone{T,R}(G, ZG, 1 + vec_dim))
+    JuMP.@objective(model, Min, h / log(R(2)))
+    JuMP.@constraint(model, [h; rho_vec] in EpiQKDTriCone{R,T}(G, ZG, 1 + vec_dim))
 
-    set_optimizer(model, Hypatia.Optimizer{T})
+    set_optimizer(model, Hypatia.Optimizer{R})
     set_attribute(model, "verbose", true)
     JuMP.optimize!(model)
     return JuMP.objective_value(model)
     #    return JuMP.solve_time(model)    
 end
+mub_rate(v::Real, d::Integer, n::Integer) = mub_rate(ComplexF64, v, d, n)
