4f82daccab4a5631430ad204e6b8a782694d937d
araujoms <maltusan@gmail.com>
2024-06-20 16:19:24 +0200

    add facial reduction and analytical solution to DMCV example

--- examples/dmcv.jl
+++ examples/dmcv.jl
@@ -20,28 +20,49 @@ end
 function integrate(bounds, pars)
     T = eltype(pars)
     problem = Integrals.IntegralProblem(integrand, bounds, pars)
-    sol = Integrals.solve(problem, Integrals.HCubatureJL(); reltol = Base.rtoldefault(T), abstol = Base.rtoldefault(T))
+    tol = T == Float64 ? eps(T)^(3 / 4) : sqrt(eps(T))
+    sol = Integrals.solve(problem, Integrals.HCubatureJL(); reltol = tol, abstol = tol)
     return sol.u
 end
 
-function hba_dmcv(::Type{T}, L::Real, ξ::Real, α::Real) where {T}
-    L, ξ, α = T.((L, ξ, α))
+function joint_probability(L::T, ξ::T, α::T) where {T<:Real}
     η = 10^(-2 * L / 100)
-    p_ab = zeros(T, 4, 4)
+    pAB = zeros(T, 4, 4)
     for x = 0:3
         pars = [ξ, η, x, α]
         for z = 0:3
             bounds = ([T(0), T(π) * (2 * z - 1) / 4], [T(Inf), T(π) * (2 * z + 1) / 4])
-            p_ab[x+1, z+1] = integrate(bounds, pars)
+            pAB[x+1, z+1] = integrate(bounds, pars)
         end
     end
-    p_ab ./= 4 * T(π) * (1 + η * ξ / 2)
-    p_ba = transpose(p_ab)
-    return conditional_entropy(p_ba)
+    pAB ./= 4 * T(π) * (1 + η * ξ / 2)
+    return pAB
 end
 
-function simulated_expectations(::Type{T}, L::Real, ξ::Real, α::Real) where {T}
-    L, ξ, α = T.((L, ξ, α))
+function hba_dmcv(L::T, ξ::T, α::T) where {T<:Real}
+    pAB = joint_probability(L, ξ, α)
+    pBA = transpose(pAB)
+    return conditional_entropy(pBA)
+end
+
+function hbe_dmcv_analytic(L::T, α::T) where {T<:Real}
+    η = 10^(-2 * L / 100)
+    c =
+        exp(-(1 - η) * α^2 / 2) / sqrt(T(2)) * [
+            sqrt(cosh((1 - η) * α^2) + cos((1 - η) * α^2)),
+            sqrt(sinh((1 - η) * α^2) + sin((1 - η) * α^2)),
+            sqrt(cosh((1 - η) * α^2) - cos((1 - η) * α^2)),
+            sqrt(sinh((1 - η) * α^2) - sin((1 - η) * α^2))
+        ]
+    eve_states = [[c[j+1] * exp(im * T(π) * i * j / 2) for j = 0:3] for i = 0:3]
+    pAB = joint_probability(L, T(0), α)
+    pA = sum(pAB; dims = 2)
+    ρE = sum(pA[x] * ketbra(eve_states[x]) for x = 1:4)
+    ρBE = sum(pAB[x, z] * kron(proj(z, 4), ketbra(eve_states[x])) for x = 1:4, z = 1:4)
+    return entropy(ρBE) - entropy(ρE)
+end
+
+function simulated_expectations(L::T, ξ::T, α::T) where {T<:Real}
     η = 10^(-2 * L / 100)
     exp_sim = zeros(T, 4, 4)
     for x = 0:3
@@ -55,21 +76,18 @@ function simulated_expectations(::Type{T}, L::Real, ξ::Real, α::Real) where {T
 end
 
 function alice_part(α::Real)
-    ρ = Hermitian(ones(Complex{typeof(α)}, 4, 4))
-    ρ.data[1, 2] = exp(-(1 + im) * α^2)
-    ρ.data[1, 3] = exp(-2 * α^2)
-    ρ.data[1, 4] = exp(-(1 - im) * α^2)
-    ρ.data[2, 3] = ρ.data[1, 2]
-    ρ.data[2, 4] = ρ.data[1, 3]
-    ρ.data[3, 4] = ρ.data[1, 2]
-    ρ *= 0.25
+    ρ = zeros(Complex{typeof(α)}, 4, 4)
+    for j = 0:3, i = 0:j
+        ρ[i+1, j+1] = 0.25 * exp(-α^2 * (1 - (1.0 * im)^(i - j)))
+    end
+    return Hermitian(ρ)
 end
 
 function sinkpi4(::Type{T}, k::Integer) where {T} #computes sin(k*π/4) with high precision
     if mod(k, 4) == 0
-        return 0
+        return T(0)
     else
-        signal = (-1)^div(k, 4, RoundDown)
+        signal = T((-1)^div(k, 4, RoundDown))
         if mod(k, 2) == 0
             return signal
         else
@@ -126,6 +144,7 @@ end
 
 function gkraus(::Type{T}, Nc::Integer) where {T}
     sqrtbasis = sqrt.(region_operators(T, Nc))
+    #    cleanup!.(sqrtbasis;tol=10^3*eps(T))
     V = sum(kron(I(4), sqrtbasis[i], ket(i, 4)) for i = 1:4)
     return V
 end
@@ -140,19 +159,13 @@ function zkraus(Nc::Integer)
     return K
 end
 
-function rate_dmcv(::Type{T}, Nc::Integer, L::Real, ξ::Real, α::Real) where {T}
-    return hbe_dmcv(T, Nc, L, ξ, α) - hba_dmcv(T, L, ξ, α)
-end
-rate_dmcv(Nc::Integer, L::Real, ξ::Real, α::Real) = rate_dmcv(Float64, Nc, L, ξ, α)
-
-function hbe_dmcv(::Type{T}, Nc::Integer, L::Real, ξ::Real, α::Real) where {T}
-    L, ξ, α = T.((L, ξ, α))
+function hbe_dmcv_general(Nc::Integer, L::T, ξ::T, α::T) where {T<:Real}
     dim_ρAB = 4 * (Nc + 1)
     model = GenericModel{T}()
     @variable(model, ρAB[1:dim_ρAB, 1:dim_ρAB], Hermitian)
 
     exp_ρAB = constraint_expectations(T, ρAB, Nc)
-    exp_sim = simulated_expectations(T, L, ξ, α)
+    exp_sim = simulated_expectations(L, ξ, α)
     @constraint(model, exp_sim .== exp_ρAB)
     ρA = partial_trace(T(1) * ρAB, 2, [4, Nc + 1])
     @constraint(model, ρA == alice_part(α))
@@ -168,8 +181,7 @@ function hbe_dmcv(::Type{T}, Nc::Integer, L::Real, ξ::Real, α::Real) where {T}
     blocks = [(i-1)*block_size+1:i*block_size for i = 1:4]
 
     vec_dim = Cones.svec_length(Complex, dim_ρAB)
-    ρAB_vec = Vector{GenericAffExpr{T,GenericVariableRef{T}}}(undef, vec_dim)
-    Cones._smat_to_svec_complex!(ρAB_vec, ρAB, sqrt(T(2)))
+    ρAB_vec = svec(ρAB, Complex{T})
 
     @variable(model, h)
     @objective(model, Min, h / log(T(2)))
@@ -181,5 +193,66 @@ function hbe_dmcv(::Type{T}, Nc::Integer, L::Real, ξ::Real, α::Real) where {T}
     return objective_value(model)
     return solve_time(model)
 end
-hbe_dmcv(Nc::Integer, L::Real, ξ::Real, α::Real) = hbe_dmcv(Float64, Nc, L, ξ, α)
-hbe_dmcv(Nc::Integer) = hbe_dmcv(Nc, 60, 0.05, 0.35)
+
+coherent(Nc::Integer, β::Number) = exp(-abs2(β) / 2) * [β^n / sqrt(factorial(n)) for n = 0:Nc]
+isometry(Nc::Integer, α::Real) = sum(kron(ket(x + 1, 4), coherent(Nc, im^x * α)) * ket(x + 1, 4)' for x = 0:3)
+function hbe_dmcv_reduced(Nc::Integer, L::T, α::T) where {T<:Real}
+    dim_σAB = 4
+    model = GenericModel{T}()
+
+    η = 10^(-2 * L / 100)
+    σAB = Matrix{Complex{T}}(undef, 4, 4)
+    for x2 = 0:3, x1 = 0:3
+        σAB[x1+1, x2+1] = 0.25 * exp(-α^2 * (1 - η) * (1 - (1.0 * im)^(x1 - x2)))
+    end
+    σAB = Hermitian(σAB)
+
+    #    V = isometry(Nc,sqrt(η)*α)
+    #    ρAB = Hermitian(V*σAB*V')
+
+    sqrtbasis = sqrt.(region_operators(T, Nc))
+    states = [sqrtbasis[k] * coherent(Nc, im^x * sqrt(η) * α) for k = 1:4, x = 0:3]
+    norms = norm.(states)
+
+    Ghat = [I(dim_σAB)]
+    Zhat = [sum(norms[k, x] * kron(proj(x, 4), ket(k, 4)) for x = 1:4) for k = 1:4]
+
+    permutation = vec(reshape(1:16, 4, 4)')
+    Zhatperm = [Zi[permutation, :] for Zi in Zhat]
+
+    block_size = 4
+    blocks = [(i-1)*block_size+1:i*block_size for i = 1:4]
+
+    vec_dim = Cones.svec_length(Complex, dim_σAB)
+    σAB_vec = svec(σAB, Complex{T})
+
+    @variable(model, h)
+    @objective(model, Min, h / log(T(2)))
+    @constraint(model, [h; σAB_vec] in EpiQKDTriCone{T,Complex{T}}(Ghat, Zhatperm, 1 + vec_dim; blocks))
+
+    set_optimizer(model, Hypatia.Optimizer{T})
+    set_attribute(model, "verbose", true)
+    optimize!(model)
+    return objective_value(model)
+    return solve_time(model)
+end
+
+function hbe_dmcv(Nc::Integer, L::T, ξ::T, α::T) where {T<:Real}
+    if ξ == 0
+        return hbe_dmcv_reduced(Nc, L, α)
+    else
+        return hbe_dmcv_general(Nc, L, ξ, α)
+    end
+end
+hbe_dmcv(Nc::Integer) = hbe_dmcv(Nc, 60.0, 0.05, 0.35)
+
+function rate_dmcv_analytic(L::T, ξ::T, α::T) where {T<:Real}
+    return hbe_dmcv_analytic(L, α) - hba_dmcv(L, ξ, α)
+end
+
+function rate_dmcv(Nc::Integer, L::T, ξ::T, α::T) where {T<:Real}
+    return hbe_dmcv(Nc, L, ξ, α) - hba_dmcv(L, ξ, α)
+end
+
+#f64 2.801905307769914e-8
+#d64 6.03065794378403782333344081012188704e-14
